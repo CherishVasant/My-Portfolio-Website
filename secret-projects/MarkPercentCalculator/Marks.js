b6fc25d4882668d64204f69aa9248e5197e14a8e
Cherish Vasant <cherish.vasant@gmail.com>
2026-08-05 10:18:21 +0530

    Update

---
+++ secret-projects/MarkPercentCalculator/Marks.js
@@ -0,0 +1,35 @@
+class Marks
+{
+    constructor(
+        name,
+        totalMarks,
+        className,
+        engLang,
+        engLit,
+        tamil,
+        math,
+        bio,
+        chem,
+        phy,
+        comp,
+        geo,
+        his,
+    ) {
+        this.name = name;
+        this.totalMarks = totalMarks;
+        this.className = className;
+
+        this.engLang = engLang;
+        this.engLit = engLit;
+        this.tamil = tamil;
+        this.math = math;
+        this.bio = bio;
+        this.chem = chem;
+        this.phy = phy;
+        this.comp = comp;
+        this.geo = geo;
+        this.his = his;
+    }
+}
+
+export default Marks
